e672b5485d66aeaba7d856dcf36a329098aff2cc
chelsealuttrall <chelsealuttrall@gmail.com>
2020-02-11 18:24:19 -0700

    Worked with aliff on nemesis actions and functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,109 +1,110 @@
-let health = 0;
-let attackPower = 0;
-let counterPower = 0;
+$(document).ready(function() {
+    let health = 0;
+    let attackPower = 0;
+    let counterPower = 0;
 
-//characters
-let babyHTML = $("#baby")
-let youngHTML = $("#young")
-let middleHTML = $("#middle")
-let oldHTML = $("#old")
-    //character traits
-let baby = {
-    cssID: "#baby",
-    health: 10,
-    attackPower: 5,
-    counterPower: 2,
-};
+    //characters
+    let babyHTML = $("#baby")
+    let youngHTML = $("#young")
+    let middleHTML = $("#middle")
+    let oldHTML = $("#old")
+        //character traits
+    let baby = {
+        cssID: "#baby",
+        health: 10,
+        attackPower: 5,
+        counterPower: 2,
+    };
 
-let young = {
-    cssID: "#young",
-    health: 12,
-    attackPower: 8,
-    counterPower: 3,
-};
+    let young = {
+        cssID: "#young",
+        health: 12,
+        attackPower: 8,
+        counterPower: 3,
+    };
 
-let middle = {
-    cssID: "#middle",
-    health: 15,
-    attackPower: 10,
-    counterPower: 1,
-};
+    let middle = {
+        cssID: "#middle",
+        health: 15,
+        attackPower: 10,
+        counterPower: 1,
+    };
 
-let old = {
-    cssID: "#old",
-    health: 8,
-    attackPower: 5,
-    counterPower: 5,
-};
+    let old = {
+        cssID: "#old",
+        health: 8,
+        attackPower: 5,
+        counterPower: 5,
+    };
 
-//arrays of characters for actions
-let deniros = [baby, young, middle, old];
-let fighters = [];
-let f = ["#baby", "#young", "#middle", "#old"];
-
-
-let player = []
-let nemesis
-let winTally = 0
-let playerHealth
-let playerAttack
-let nemesisHealth
-let nemesisCounter
-let newPlayerHealth
-let newNemesisHealth
-
-$("#winTally").append(`  ` + winTally)
+    //arrays of characters for actions
+    let deniros = [baby, young, middle, old];
+    let fighters = [];
+    let f = ["#baby", "#young", "#middle", "#old"];
 
 
-for (let i = 0; i < deniros.length; i++) {
+    let player = []
+    let nemesis
+    let winTally = 0
+    let playerHealth
+    let playerAttack
+    let nemesisHealth
+    let nemesisCounter
+    let newPlayerHealth
+    let newNemesisHealth
 
-    $(deniros[i].cssID).click(function() {
+    $("#winTally").append(`  ` + winTally)
 
-        if (fighters.length < 2) {
 
-            let id = `#${this.id}`
-            $("#choose").css({ "text-decoration": "line-through" })
-            $(id).appendTo("#battleground")
-            fighters.push(id)
-            console.log(fighters)
+    for (let i = 0; i < deniros.length; i++) {
 
-            f = f.filter(function(element) {
-                return element !== id;
-            })
-            for (let j = 0; j < f.length; j++) {
-                $(f[j]).appendTo("#theBench")
+        $(deniros[i].cssID).click(function() {
+            // if the first two cards are selected, move to battle ground
+            if (fighters.length < 2) {
+
+                let id = `#${this.id}`
+                $("#choose").css({ "text-decoration": "line-through" })
+                $(id).appendTo("#battleground")
+                fighters.push(id)
+                console.log(fighters)
+
+                f = f.filter(function(element) {
+                    return element !== id;
+                })
+                for (let j = 0; j < f.length; j++) {
+                    $(f[j]).appendTo("#theBench")
+                }
             }
-        }
-        if (fighters.length < 2) {
-            player = fighters[0] //.replace("#", "")]
-            console.log(player)
-            playerHealth = deniros[i].health
-            playerAttack = deniros[i].attackPower
-            $("#myHealthHTML").text(playerHealth)
-        } else if (fighters.length < 3) {
-            let nemesis = fighters[1]
-            console.log(nemesis)
-            nemesisHealth = deniros[i].health
-            nemesisCounter = deniros[i].counterPower
-            $("#nemesisHealthHTML").text(nemesisHealth)
-        } else {
-            console.log("no action")
-        }
-        return nemesisCounter
-        return playerAttack;
-    })
-};
+            if (fighters.length < 2) {
+                player = fighters[0] //.replace("#", "")]
+                console.log(player)
+                playerHealth = deniros[i].health
+                playerAttack = deniros[i].attackPower
+                $("#myHealthHTML").text(playerHealth)
+            } else if (fighters.length < 3) {
+                let nemesis = fighters[1]
+                console.log(nemesis)
+                nemesisHealth = deniros[i].health
+                nemesisCounter = deniros[i].counterPower
+                $("#nemesisHealthHTML").text(nemesisHealth)
+            } else {
+                console.log("no action")
+            }
+            return nemesisCounter
+            return playerAttack;
+        })
+    };
 
-console.log(f)
+    console.log(f)
 
-//var shootout = $(function() {});
+    //var shootout = $(function() {});
 
-//console.log($(playerHealth))
+    //console.log($(playerHealth))
 
-let regPlay = function() {
+    let regPlay = function() {
 
-    console.log("button")
-    $(function() {
+        console.log("button")
+
         let nowPlayerHealth = $("#myHealthHTML").text()
             //console.log(nowPlayerHealth)
         let nowNemesisHealth = $("#nemesisHealthHTML").text()
@@ -116,38 +117,47 @@ let regPlay = function() {
         console.log(newNemesisHealth)
         $("#myHealthHTML").text(newPlayerHealth)
         $("#nemesisHealthHTML").text(newNemesisHealth);
-    })
-}
+    }
 
 
 
 
-let gameOver = function() {
-    $("#myHealthHTML").text("GAME OVER")
-        //console.log($("#myHealthHTML").text())
-    $("div#button").hide();
-    //if I have time, I'll make this appear on a pop up or something
-    console.log("Choke, choke, gurgle, gurgle, mlehhhhh");
-}
+    let gameOver = function() {
+        $("#myHealthHTML").text("GAME OVER")
+            //console.log($("#myHealthHTML").text())
+        $("div#button").hide();
+        //if I have time, I'll make this appear on a pop up or something
+        console.log("Choke, choke, gurgle, gurgle, mlehhhhh");
+    };
 
-//start here on my win function
-
-
+    //start here on my win function
 
 
-$(document).ready(function() {
+
+
     $("#button").click(function() {
+
+        regPlay();
         let h = $("#myHealthHTML").text();
         let n = $("#nemesisHealthHTML").text()
-        console.log(h)
-        if (h > 1) {
-            return regPlay()
+        console.log("Nemesis health", n)
+        if (h <= 0) {
+            return gameOver();
         } else if (n < 1) {
             $("div#button").hide()
-            $(winTally).val() += 1
+            winTally += 1
+            $("#winTally").text(winTally)
             $("#nemesisHealthHTML").text("Choke, choke, gurgle, gurle, mlehhhhhh --- You win!")
-        } else {
-            return gameOver()
+            let nem = fighters[1];
+
+            fighters = fighters.filter(function(fighter) {
+                return fighter != nem;
+            })
+            console.log(nem)
+            console.log(fighters);
+            $(nem).hide();
+            $("div#button").show()
+
         }
     })
 });
